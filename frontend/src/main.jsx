e5cd16e8b8e5701a7ae6d9619472b93cfff29aca
Princewill Isonguyo <princewillisonguyo@gmail.com>
2026-09-24 05:29:40 -0700

    new main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,1 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } fro "reat-router;-dom";
 
+import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
+
+import "./index.css";
+
+ReactDom.creaeRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+  <BrowserRouter>
+  <AuthProvider>
+  <App />
+  </AuthProvider>
+  </BrowserRouter>
+  </React.StrictMode
+);
